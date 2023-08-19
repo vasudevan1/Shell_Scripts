2a670c5f4e467c4fafd8ac7f568ca47434eb610c
vasudevan1 <pasupuletivasudevan@gmail.com>
2023-08-19 10:09:13 +0530

    First commit it Shell script

---
+++ 01.Hello-World.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "This is first shell script program i am writing to print date and time"
+R="\e[31m"
+G="\e[40m"
+G="\e[32m"
+N="\e[0m"
+P="\e[34m"
+DATE=$(date +%F)
+echo -e "Today date is $G $DATE"
