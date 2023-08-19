97ac213c00673d7f963842ac00017f59144eaf93
vasudevan1 <pasupuletivasudevan@gmail.com>
2023-08-19 12:16:25 +0530

    Modified Pallidrome shell script

--- 03.Palindrome.sh
+++ 03.Palindrome.sh
@@ -1,10 +1,35 @@
+# #!/bin/bash
+# #Cheking the weather given string is Palindrome or not.
+# echo "Enter string"
+# read string #Reading string from console
+# if [ [ echo "$string" |rev == $string ] ]
+#     then
+#         echo "Given $string is Palindrome
+#     else
+#         Echo "Given $string is not Palindrome
+# fi
+
 #!/bin/bash
-#Cheking the weather given string is Palindrome or not.
-echo "Enter string"
-read string #Reading string from console
-if [ [ $(rev <<< "$string") == $string ] ]
-    then
-        echo "Given $string is Palindrome
-    else
-        Echo "Given $string is not Palindrome
-fi
+# Shell script to test if a string is a palindrome
+# This uses the well known method to scan half the string matching
+# the i and len - i chars, if one mismatch is found it quits immediately
+
+echo "Enter a String : "
+read string
+
+i=0
+len=${#string}
+
+#get the mid value upto which the comparison would be done
+mid=$(($len/2))
+
+while [ $i -lt $mid ]
+  do
+    if [ "${string:$i:1}" != "${string: -$(($i+1)):1}" ]
+      then
+      echo "\"$string\" is NOT a Palindrome"
+      exit
+    fi
+    i=$(($i+1))
+done
+echo "\"$string\" is a Palindrome"
