bae208901f2d726fc1af48be801a53a09ab6a2f1
vasudevan1 <pasupuletivasudevan@gmail.com>
2023-08-19 11:11:31 +0530

    Adding if condition shell script

--- 02.Conditions.sh
+++ 02.Conditions.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Learning the If Condition
+echo "Enter two numbers to verify to check maximum value"
 read -s num1
 read -s num2
-if [$num1 -gt $num2]
+if [ $num1 -gt $num2 ]
 then 
-    echo "$num1 is greaterthan $num2"
+    echo "Maximum value is: $num1"
+    #echo "$num1 is greaterthan $num2"
 else
-    echo "$num2 is greaterthan $num1"
+    echo "Maximum value is: $num2"
+    #echo "$num2 is greaterthan $num1"
 fi
